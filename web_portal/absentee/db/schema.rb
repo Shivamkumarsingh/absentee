2d07b408fa9ae408e4be1a86dd088d5cdad5235a
tanya-saroha <tanya@joshsoftware.com>
2019-04-26 20:17:33 +0530

    Added User, Devise and haml gem

--- web_portal/absentee/db/schema.rb
+++ web_portal/absentee/db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_141957) do
+ActiveRecord::Schema.define(version: 2019_04_26_144518) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
-  enable_extension "uuid-ossp"
 
   create_table "klasses", force: :cascade do |t|
     t.bigint "school_id"
@@ -54,6 +52,18 @@ ActiveRecord::Schema.define(version: 2019_04_26_141957) do
     t.datetime "updated_at", null: false
     t.index ["klass_id"], name: "index_students_on_klass_id"
     t.index ["section_id"], name: "index_students_on_section_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "klasses", "schools"
