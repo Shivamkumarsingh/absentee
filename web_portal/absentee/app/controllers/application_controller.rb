1dc9cfdc67847420b4d456ca19f7cbb85fd7e8ad
tanya-saroha <tanya@joshsoftware.com>
2019-04-27 09:43:52 +0530

    Added Log out button

--- web_portal/absentee/app/controllers/application_controller.rb
+++ web_portal/absentee/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+	before_action :authenticate_user!, unless: -> { params[:controller].eql?("home") || params[:action].eql?('forgot_password') }
+
 	def load_school_sections
 		school = School.last
 		@sections = Section.joins(:klass).where("klasses.school_id": school.id).select('sections.name, title, klasses.id as klass_id, sections.id as id')
