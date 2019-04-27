5fac1d7e249d0f13f22a43caf255f9291dcea0a9
monika <monika.prajapati@joshsoftware.com>
2019-04-27 15:21:54 +0530

    updated attendance apis

--- web_portal/absentee/app/controllers/attendances_controller.rb
+++ web_portal/absentee/app/controllers/attendances_controller.rb
@@ -7,7 +7,8 @@ class AttendancesController < ApplicationController
     section_id = params[:section_id]
     klass_id = Section.find(section_id).klass_id
     absent_array = params[:attendance]
-    absent_array.each do |student_id|
+    absent_array.each do |roll|
+      student_id = Student.where(roll_number: roll).where(section_id: section_id).first.id
       @attendance = Attendance.find_or_create_by(student_id: student_id, present: false,
        section_id: section_id, klass_id: klass_id, date: Date.today)
     end
@@ -29,9 +30,10 @@ class AttendancesController < ApplicationController
     attendance_hash = params[:attendance].reject {|key, value| value == "" }
     attendance_hash.each do |attendance|
       klass_id = Section.find(attendance.first).klass_id
-      student_ids = attendance.second.split(',').map(&:strip).reject(&:blank?)
-      student_ids.each do |id|
-        Attendance.find_or_create_by!(section_id: attendance.first, student_id: id, date: Date.today,
+      roll_ids = attendance.second.split(',').map(&:strip).reject(&:blank?)
+      roll_ids.each do |id|
+        student_id = Student.where(roll_number: id).where(section_id: attendance.first).first
+        Attendance.find_or_create_by!(section_id: attendance.first, student_id: student.first, date: Date.today,
         present: false, klass_id: klass_id)
       end
     end
@@ -41,7 +43,7 @@ class AttendancesController < ApplicationController
   def todays_attendance
     section_id = params[:section_id].to_i
     @attendance = Student.joins("LEFT JOIN attendances ON attendances.student_id = students.id AND attendances.date::Date = '#{Date.today}'").
-                          where(students: {section_id: 321}).
+                          where(students: {section_id: params[:section_id]}).
                           select('attendances.present as present, name, students.roll_number, students.id')
     respond_to do |format|
       format.json { render json: @attendance }
